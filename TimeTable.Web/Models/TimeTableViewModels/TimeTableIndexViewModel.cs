6bf908e87fadd08527871fedfbc00e99e7b51e72
markarugo <cadin@freemail.hu>
2018-04-23 12:38:46 +0200

    gitignore

---
+++ TimeTable.Web/Models/TimeTableViewModels/TimeTableIndexViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TimeTableDesigner.Web.Models.TimeTableViewModels
+{
+    public class TimeTableIndexViewModel
+    {
+        [DisplayName("Név")]
+        public string Name { get; set; }
+
+        [DisplayName("Félév")]
+        public string Semester { get; set; }
+
+        [DisplayName("Kurzusok száma")]
+        public int CoursesCount { get; set; }
+    }
+}
